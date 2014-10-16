8782da248dce776c7b61e96cfd9643d0cedc086f
David Hemminger <drh22@duke.edu>
2014-10-16 23:36:34 +0000

    edits to createStudyGroups.sql

--- addStartingData.sql
+++ addStartingData.sql
@@ -21,6 +21,19 @@ INSERT INTO PersonAdminOfGroup VALUES
   (6,2),
   (9,3);
 
+INSERT INTO PersonMemberOfGroup VALUES
+  (1,1),
+  (1,2),
+  (6,2),
+  (9,3),
+  (2,2),
+  (7,2),
+  (3,1),
+  (4,1),
+  (8,3),
+  (10,3),
+  (5,3);
+
 INSERT INTO GroupJoinRequest VALUES
   (1,2,2,'approved','2014-10-11 10:23:54','2014-10-11 16:27:34','1',NULL),
   (2,8,2,'denied','2014-09-15 09:13:11','2014-09-24 11:30:01','6','Please please please let me join this group!'),
@@ -43,7 +56,10 @@ INSERT INTO Thread VALUES
 INSERT INTO Post VALUES
   (1,1,9,'When should we meet for the MCAT Study Session?  Any of Tuesday, Wednesday, or Friday next week would work for me.','2014-10-13 10:53:11'),
   (2,1,8,'See title.','2014-10-14 18:00:05'),
-
+  (1,2,8,'Tuesday or Wednesday would work for me!','2014-10-14 15:56:00'),
+  (1,3,10,'None of those days really work for me, sorry!','2014-10-14 18:54:11'),
+  (1,4,5,'Wednesday would be best for me, but I could also do Tuesday','2014-10-15 09:11:53'),
+  
 
 
 INSERT INTO PersonAttendingEvent VALUES
@@ -52,14 +68,15 @@ INSERT INTO PersonAttendingEvent VALUES
   (6,2,'attending'),
   (3,1,'attending'),
   (5,2,'attending'),
-  (7,1,'not_attending'),
-  (5,1,'not_attending');
+  (3,1,'not_attending'),
+  (5,1,'attending');
 
 INSERT INTO ThreadTaggedWithGroup VALUES
   (1,3),
+  (2,2);
 
 INSERT INTO ThreadTaggedWithEvent VALUES
-  (1,1),
+  (1,1);
 
 INSERT INTO EventTaggedWithGroup VALUES
   (1,3),
--- createStudyGroups.sql
+++ createStudyGroups.sql
@@ -1,8 +1,7 @@
-CREATE TABLE StudyGroup(
-	gid INTEGER NOT NULL PRIMARY KEY,
+CREATE TABLE Topic(
+	tid INTEGER NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
-	accessibility VARCHAR(7) NOT NULL
-		CHECK(accessibility = 'private' OR accessibility = 'open')
+	description VARCHAR(300)
 );
 
 CREATE TABLE Person(
@@ -12,9 +11,43 @@ CREATE TABLE Person(
 	email VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE Request(
+	rid INTEGER NOT NULL PRIMARY KEY,
+	pid INTEGER NOT NULL REFERENCES Person(pid),
+	status VARCHAR(10) NOT NULL
+		CHECK(status = 'matched' OR status = 'unmatched'),
+	
+CREATE TABLE TimeSlot(
+	tid INTEGER NOT NULL PRIMARY KEY,
+	time_slot_date DATE NOT NULL,
+	time_slot_time VARCHAR(10)
+		CHECK(time_slot_time = 'morning' OR time_slot_time = 'afternoon' OR time_slot_time = 'evening' OR time_slot_time = 'night')
+);
+
+CREATE TABLE RequestTimes(
+	rid INTEGER NOT NULL REFERENCES Request(rid),
+	tid INTEGER NOT NULL REFERENCES TimeSlot(tid),
+	PRIMARY KEY(rid,tid)
+);
+
+CREATE TABLE StudyGroup(
+	gid INTEGER NOT NULL PRIMARY KEY,
+	meeting_time INTEGER NOT NULL REFERENCES TimeSlot(tid)
+);
+
+
+
+--ignore things below here for now--
+
 CREATE TABLE PersonAdminOfGroup(
 	pid INTEGER NOT NULL REFERENCES Person(pid),
 	gid INTEGER NOT NULL REFERENCES StudyGroup(gid),
+	PRIMARY KEY(pid,gid)
+);
+
+CREATE TABLE PersonMemberOfGroup(
+	pid INTEGER NOT NULL REFERENCES Person(pid),
+	gid INTEGER NOT NULl REFERENCES StudyGroup(gid),
 	PRIMARY KEY(pid,gid)
 );
 
