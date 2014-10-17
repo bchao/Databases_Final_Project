40bba423dc41a264240f3374388e6bc7c1dab6b9
Brandon Chao <brandonchao3@gmail.com>
2014-10-16 21:12:21 -0400

    More examples

--- sampleQueries.sql
+++ sampleQueries.sql
@@ -4,4 +4,5 @@ SELECT * FROM Meeting WHERE topic = (SELECT tid FROM Topic WHERE name = 'Studyin
 --List of names/emails of everyone attending meeting with mid = 1
 SELECT first_name, last_name, email FROM Person WHERE pid IN (SELECT pid FROM PersonAttendingMeeting WHERE mid = 1);
 
---List all scheduled sessions for user 'Leonard Walworth'
+--List all scheduled sessions with userID '6'
+SELECT * FROM Meeting WHERE id IN (SELECT mid from PersonAttendingMeeting WHERE pid = 6)
