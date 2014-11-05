9bba9b2182d56199a89eb6657e5d47d637afb6d3
Brandon Chao <brandonchao3@gmail.com>
2014-11-04 22:47:58 -0500

    udpate schema

--- createStudyGroups.sql
+++ createStudyGroups.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Topic(
-	tid INTEGER NOT NULL PRIMARY KEY,
+	topid INTEGER NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(300)
 );
@@ -14,14 +14,14 @@ CREATE TABLE Person(
 CREATE TABLE Request(
 	rid INTEGER NOT NULL PRIMARY KEY,
 	pid INTEGER NOT NULL REFERENCES Person(pid),
-	tid INTEGER NOT NULL REFERENCES Topic(tid),
+	topid INTEGER NOT NULL REFERENCES Topic(topid),
 	time TIMESTAMP NOT NULL,
 	status VARCHAR(9) NOT NULL
 		CHECK(status = 'open' OR status = 'closed')
 );
 	
 CREATE TABLE TimeSlot(
-	tid INTEGER NOT NULL PRIMARY KEY,
+	tsid INTEGER NOT NULL PRIMARY KEY,
 	time_slot_date DATE NOT NULL,
 	time_slot_time VARCHAR(9)
 		CHECK(time_slot_time = 'morning' OR time_slot_time = 'afternoon' OR time_slot_time = 'evening' OR time_slot_time = 'night')
@@ -29,14 +29,14 @@ CREATE TABLE TimeSlot(
 
 CREATE TABLE RequestTimes(
 	rid INTEGER NOT NULL REFERENCES Request(rid),
-	tid INTEGER NOT NULL REFERENCES TimeSlot(tid),
-	PRIMARY KEY(rid,tid)
+	tsid INTEGER NOT NULL REFERENCES TimeSlot(tsid),
+	PRIMARY KEY(rid,tsid)
 );
 
 CREATE TABLE Meeting(
 	mid INTEGER NOT NULL PRIMARY KEY,
-	topic INTEGER NOT NULL REFERENCES Topic(tid),
-	meeting_time INTEGER NOT NULL REFERENCES TimeSlot(tid)
+	topic INTEGER NOT NULL REFERENCES Topic(topid),
+	meeting_time INTEGER NOT NULL REFERENCES TimeSlot(tsid)
 );
 
 CREATE TABLE PersonAttendingMeeting(
