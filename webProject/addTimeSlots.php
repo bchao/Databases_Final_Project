dbcaf728f97349ef14c682e64bddd5ca540e83e5
David Hemminger <drh22@duke.edu>
2014-12-06 15:33:39 -0500

    added addTimeSlots.php

---
+++ webProject/addTimeSlots.php
@@ -0,0 +1,32 @@
+<?php
+ require("config.php");
+
+ $date = '2014-12-06';
+ $end_date = '2018-12-31';
+
+ $times = array('morning', 'afternoon', 'evening', 'night');
+ 
+ while (strtotime($date) <= strtotime($end_date)) {
+
+ 	foreach ($times as $time) {
+ 		$query = "
+		INSERT INTO TimeSlot 
+		VALUE(:time_slot_date, :time_slot_time)
+		";
+		
+		$query_params = array(
+			':time_slot_date' => $date,
+			':time_slot_time' => $time
+		);
+
+		try{
+			$stmt = $db->prepare($query);
+			$result = $stmt->execute($query_params);
+		}
+		catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
+ 	}
+
+ 	$date = date ("Y-m-d", strtotime("+1 day", strtotime($date)));
+ }
+ 
+?>
