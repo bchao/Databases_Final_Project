b45aac5be6bcf462f00bc4fb6879b31305f4ee43
David Hemminger <drh22@duke.edu>
2014-12-07 14:49:22 -0500

    filled out deleteRequest.php

--- webProject/deleteRequest.php
+++ webProject/deleteRequest.php
@@ -1,4 +1,18 @@
 <?php
 	require("config.php");
 
+	$query = "
+			DELETE FROM Request
+			WHERE rid = :rid
+		";
+		$query_params = array(
+			':rid' => $_POST['rid']
+		);
+
+		try{
+			$stmt = $db->prepare($query);
+			$stmt->execute($query_params);
+		}
+
+		catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
 ?>
